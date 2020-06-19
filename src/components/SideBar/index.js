45a6325e77c4891eff3dc69ca244bac58471cd36
Sanjay Verma <svsanjayverma05@gmail.com>
2020-06-19 12:30:03 +0530

    test commit 2 ngrok

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import FilterSidebar from "Components/FilterSidebar";
+import {useDispatch, useSelector} from "react-redux";
 
 const SideBar = () => {
+	const dispatch = useDispatch();
+	const {range} = useSelector(state=>state.items);
 	return (
 		<div className="shopping-cart__sidebar">
-			<FilterSidebar />
+			<FilterSidebar dispatch={dispatch} range={range} />
 		</div>
 	);
 };

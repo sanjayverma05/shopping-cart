6d8dab9a3be61d4b70632349149dff3506f64355
Sanjay Verma <svsanjayverma05@gmail.com>
2020-06-19 12:31:10 +0530

    test commit -3 ngrok

--- src/components/SortBar/index.js
+++ src/components/SortBar/index.js
@@ -1,42 +1,48 @@
 import React, { useContext, useState } from "react";
+import {useSelector,useDispatch} from "react-redux";
+import { sortItems } from "Stores/shoppingStore/actions";
+import { SORT_HIGH_TO_LOW, SORT_LOW_TO_HIGH, SORT_DISCOUNT } from "Stores/shoppingStore/actionTypes";
 
 import { AppContext } from "Components/App";
 
 import "./index.scss";
 
 const SortBar = () => {
-  const { sortItems, sortOrder } = useContext(AppContext);
+  // const { sortItems, sortOrder } = useContext(AppContext);
+  const {sortOrder} = useSelector(state=>state.items);
+  const dispatch= useDispatch()
 
   return (
-    <div className="sortbar">
-      <span className="sortbar__heading">Sort By</span>
-      <a
-        className={`sortbar__sort-option ${
-          sortOrder === "desc" ? "sortbar__sort-option--active" : ""
-        }`}
-        onClick={() => {
-          sortItems("desc");
-        }}
-      >
-        Price -- High Low
-      </a>
-      <a
-        className={`sortbar__sort-option ${
-          sortOrder === "asc" ? "sortbar__sort-option--active" : ""
-        }`}
-        onClick={() => sortItems("asc")}
-      >
-        Price -- Low High
-      </a>
-      <a
-        className={`sortbar__sort-option ${
-          sortOrder === "discount" ? "sortbar__sort-option--active" : ""
-        }`}
-        onClick={() => sortItems("discount")}
-      >
-        Discount
-      </a>
-    </div>
+		<div className="sortbar">
+			<span className="sortbar__heading">Sort By</span>
+			<a
+				className={`sortbar__sort-option ${
+					sortOrder === SORT_HIGH_TO_LOW ? "sortbar__sort-option--active" : ""
+				}`}
+				onClick={() => {
+					dispatch(sortItems(SORT_HIGH_TO_LOW));
+				}}
+				tabIndex={0}
+			>
+				Price -- High Low
+			</a>
+			<a
+				className={`sortbar__sort-option ${
+					sortOrder === SORT_LOW_TO_HIGH ? "sortbar__sort-option--active" : ""
+				}`}
+				onClick={() => dispatch(sortItems(SORT_LOW_TO_HIGH))}
+				tabIndex={0}
+			>
+				Price -- Low High
+			</a>
+			<a
+				className={`sortbar__sort-option ${sortOrder === SORT_DISCOUNT ? "sortbar__sort-option--active" : ""}`}
+				onClick={() => dispatch(sortItems(SORT_DISCOUNT))}
+				tabIndex={0}
+			>
+				Discount
+			</a>
+		</div>
   );
 };
 export default SortBar;
